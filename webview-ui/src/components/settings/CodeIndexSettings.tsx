8cbd6874e2c34c0c7c027f88fbb2dc274b60167c
Utharn Buranasaksee <utarn@yahoo.com>
2025-06-09 18:14:56 +0700

    Fix modelharbor embeddings

--- webview-ui/src/components/settings/CodeIndexSettings.tsx
+++ webview-ui/src/components/settings/CodeIndexSettings.tsx
@@ -495,7 +495,7 @@ export const CodeIndexSettings: React.FC<CodeIndexSettingsProps> = ({
 						</div>
 						<div className="mt-1">
 							<VSCodeLink
-								href="https://www.modelharbor.com/home/qdrant-setup"
+								href="https://www.modelharbor.com/qdrant-setup"
 								style={{ fontSize: "0.875rem" }}>
 								{t("settings:codeIndex.qdrantSetupLabel")}
 							</VSCodeLink>
