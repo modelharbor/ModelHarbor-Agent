aea0791a1dd1d03684d073f78a81b03f7a5897ff
Utharn Buranasaksee <utarn@yahoo.com>
2025-07-09 21:03:40 +0700

    Update new embedder

--- src/core/webview/webviewMessageHandler.ts
+++ src/core/webview/webviewMessageHandler.ts
@@ -1989,6 +1989,12 @@ export const webviewMessageHandler = async (
 						settings.codebaseIndexGeminiApiKey,
 					)
 				}
+				if (settings.codeIndexModelHarborApiKey !== undefined) {
+					await provider.contextProxy.storeSecret(
+						"codeIndexModelHarborApiKey",
+						settings.codeIndexModelHarborApiKey,
+					)
+				}
 
 				// Send success response first - settings are saved regardless of validation
 				await provider.postMessageToWebview({
@@ -2081,6 +2087,7 @@ export const webviewMessageHandler = async (
 				"codebaseIndexOpenAiCompatibleApiKey",
 			))
 			const hasGeminiApiKey = !!(await provider.context.secrets.get("codebaseIndexGeminiApiKey"))
+			const hasModelHarborApiKey = !!(await provider.context.secrets.get("codeIndexModelHarborApiKey"))
 
 			provider.postMessageToWebview({
 				type: "codeIndexSecretStatus",
@@ -2089,6 +2096,7 @@ export const webviewMessageHandler = async (
 					hasQdrantApiKey,
 					hasOpenAiCompatibleApiKey,
 					hasGeminiApiKey,
+					hasModelHarborApiKey,
 				},
 			})
 			break
--- src/shared/WebviewMessage.ts
+++ src/shared/WebviewMessage.ts
@@ -257,6 +257,7 @@ export interface WebviewMessage {
 		codeIndexQdrantApiKey?: string
 		codebaseIndexOpenAiCompatibleApiKey?: string
 		codebaseIndexGeminiApiKey?: string
+		codeIndexModelHarborApiKey?: string
 	}
 }
 
