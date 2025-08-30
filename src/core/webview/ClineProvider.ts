9679fa24aa63026d6d8ae584d76de460a6f4b359
Utharn Buranasaksee <utarn@yahoo.com>
2025-08-30 11:48:53 +0700

    Fix localization

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -972,7 +972,7 @@ export class ClineProvider
 						window.AUDIO_BASE_URI = "${audioUri}"
 						window.MATERIAL_ICONS_BASE_URI = "${materialIconsUri}"
 					</script>
-					<title>Roo Code</title>
+					<title>ModelHarbor Agent</title>
 				</head>
 				<body>
 					<div id="root"></div>
@@ -1045,7 +1045,7 @@ export class ClineProvider
 				window.AUDIO_BASE_URI = "${audioUri}"
 				window.MATERIAL_ICONS_BASE_URI = "${materialIconsUri}"
 			</script>
-            <title>Roo Code</title>
+            <title>ModelHarbor Agent</title>
           </head>
           <body>
             <noscript>You need to enable JavaScript to run this app.</noscript>
